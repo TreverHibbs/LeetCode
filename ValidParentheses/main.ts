1f4ca297d1b16f0bd00ca18690e15965d39b6782
TreverHibbs <treverhibbs@gmail.com>
2026-02-22 09:18:13 -0800

    complete correct paren

--- ValidParentheses/main.ts
+++ ValidParentheses/main.ts
@@ -1,6 +1,31 @@
 function isValid(s: string): boolean {
-	return false
-};
+	let stack: string[] = []
+	for (let i = 0; i < s.length; i++) {
+		if (s[i] == ")") {
+			let bracketToTest = stack.pop()
+			if (bracketToTest != "(") {
+				return false
+			}
+		} else if (s[i] == "]") {
+			let bracketToTest = stack.pop()
+			if (bracketToTest != "[") {
+				return false
+			}
+		} else if (s[i] == "}") {
+			let bracketToTest = stack.pop()
+			if (bracketToTest != "{") {
+				return false
+			}
+		} else {
+			stack.push(s[i])
+		}
+	}
+	if (stack.length == 0) {
+		return true
+	} else {
+		return false
+	}
+}
 
 if (isValid("()") == true) {
 	console.log("success")
@@ -28,6 +53,12 @@ if (isValid("([])") == true) {
 }
 
 if (isValid("([)]") != true) {
+	console.log("success")
+} else {
+	console.log("fail")
+}
+
+if (isValid("[") != true) {
 	console.log("success")
 } else {
 	console.log("fail")
